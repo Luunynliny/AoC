38d134c350248cfbac577bdbec8779246c40fcbc
Eccsx <thomasmattone.perso@gmail.com>
2023-12-01 17:41:05 +0100

    day 1 - part one

---
+++ 2023/day_1/day_1_part_one.py
@@ -0,0 +1,19 @@
+import re
+
+example = "./part_one_example.txt"
+puzzle = "./puzzle_input.txt"
+
+with open(puzzle) as f:
+    lines = f.readlines()
+
+calibration_sum = 0
+
+for line in lines:
+    digits = re.findall(r"\d", line)
+
+    if len(digits) == 1:
+        calibration_sum += int(digits[0]) * 11
+    else:
+        calibration_sum += int(digits[0]) * 10 + int(digits[-1])
+
+print(calibration_sum)
