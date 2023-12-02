1cc0b870fc63c7b5529f3e57dddf33ea566e01b1
Eccsx <thomasmattone.perso@gmail.com>
2023-12-02 16:29:38 +0100

    day 2 part two

---
+++ 2023/day_2/day_2_part_two.py
@@ -0,0 +1,27 @@
+import re
+
+example = "./example.txt"
+puzzle = "./puzzle_input.txt"
+
+with open(puzzle) as f:
+    lines = f.read().splitlines()
+
+game_power_sum = 0
+
+for game in lines:
+    game_split = re.split(r":|,|;", game)
+
+    cube_color_max = {"red": 0, "green": 0, "blue": 0}
+
+    for cube_subset in game_split[1:]:
+        amount, color = cube_subset.split()
+        cube_color_max[color] = max(cube_color_max[color], int(amount))
+
+    game_power = (
+        cube_color_max["red"]
+        * cube_color_max["green"]
+        * cube_color_max["blue"]
+    )
+    game_power_sum += game_power
+
+print(game_power_sum)
