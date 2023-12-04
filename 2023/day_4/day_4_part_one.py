f8cc78bac38985ce0fcbe3f53cd060b8d33adefb
Eccsx <thomasmattone.perso@gmail.com>
2023-12-04 10:51:54 +0100

    day 4 part one

---
+++ 2023/day_4/day_4_part_one.py
@@ -0,0 +1,22 @@
+from icecream import ic
+
+example = "./example.txt"
+puzzle = "./puzzle.txt"
+
+with open(puzzle) as f:
+    lines = f.read().splitlines()
+
+points_total = 0
+
+for line in lines:
+    l_split = line.split("|")
+
+    winning_numbers = set(l_split[0].split()[2:])
+    numbers = set(l_split[1].split())
+
+    matches = winning_numbers & numbers
+
+    if len(matches) > 0:
+        points_total += 2 ** (len(matches) - 1)
+
+ic(points_total)
