3b9bfba3b17495e4ca51a2a5f70ee1f4c95e7ee7
Eccsx <thomasmattone.perso@gmail.com>
2023-12-05 17:23:44 +0100

    day 5 part two

---
+++ 2023/day_5/day_5_part_two.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+example = "./example.txt"
+puzzle = "./puzzle.txt"
+
+with open(puzzle) as f:
+    lines = f.read()
+
+seed_ranges_input, *maps_input = lines.split("\n\n")
+seed_ranges = list(map(int, seed_ranges_input.split()[1:]))
+
+seed_generator = (
+    s
+    for i in range(0, len(seed_ranges), 2)
+    for s in range(seed_ranges[i], seed_ranges[i] + seed_ranges[i + 1])
+)
+
+seeds = np.fromiter(seed_generator, int)
+seeds_count = len(seeds)
+
+for mi in maps_input:
+    incr = np.zeros(seeds_count, dtype=int)
+
+    for data in mi.split("\n")[1:]:
+        dest, src, r = [int(n) for n in data.split()]
+
+        mask = (src <= seeds) & (seeds <= src + (r - 1))
+        incr[mask] = dest - src
+
+    seeds += incr
+
+print(seeds.min())
