98ef1851f06697aaacf5b15ea0c5ad3a6adcb2e7
Luunynliny <thomasmattone.perso@gmail.com>
2023-12-09 17:48:51 +0100

    day 1 part two

---
+++ 2015/01_Not_Quite_Lisp/part_two.py
@@ -0,0 +1,25 @@
+from time import perf_counter
+
+from icecream import ic
+
+puzzle = "./puzzle.txt"
+
+timer = perf_counter()
+
+with open(puzzle) as f:
+    line = f.read()
+
+floor_number = 0
+basement_enter_position = 0
+
+for i, c in enumerate(line):
+    floor_number += 1 if c == "(" else -1
+
+    if floor_number == -1:
+        basement_enter_position = i + 1
+        break
+
+ic(basement_enter_position)
+
+exec_time = perf_counter() - timer
+ic(exec_time)
