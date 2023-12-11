dcce4ef2689ab436cd60fbaf75eccc3181e82d5e
Luunynliny <thomasmattone.perso@gmail.com>
2023-12-11 03:11:55 +0100

    day 9 part one

---
+++ 2015/09_All_in_a_Single_Night/part_one.py
@@ -0,0 +1,30 @@
+from itertools import pairwise, permutations
+from time import perf_counter
+
+from icecream import ic
+
+example = "./example.txt"
+puzzle = "./puzzle.txt"
+
+timer = perf_counter()
+
+adj_matrix = {}
+
+with open(puzzle) as f:
+    for line in f.read().splitlines():
+        s, _, d, *_, w = line.split()
+
+        adj_matrix.setdefault(s, dict())[d] = int(w)
+        adj_matrix.setdefault(d, dict())[s] = int(w)
+
+shortest_route_distance = min(
+    [
+        sum(adj_matrix[u][v] for u, v in pairwise(p))
+        for p in permutations(adj_matrix)
+    ]
+)
+
+ic(shortest_route_distance)
+
+exec_time = perf_counter() - timer
+ic(exec_time)
