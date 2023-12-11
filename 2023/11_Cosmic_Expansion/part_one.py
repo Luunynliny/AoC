c8b7c1afc59654d242399ff6f384d4068e241519
Luunynliny <thomasmattone.perso@gmail.com>
2023-12-11 17:30:34 +0100

    day 11 part one

---
+++ 2023/11_Cosmic_Expansion/part_one.py
@@ -0,0 +1,37 @@
+from itertools import combinations
+from time import perf_counter
+
+import numpy as np
+from icecream import ic
+
+example = "./example.txt"
+puzzle = "./puzzle.txt"
+
+timer = perf_counter()
+
+with open(puzzle) as f:
+    lines = f.read().splitlines()
+
+image = np.array([list(l) for l in lines])
+galaxy_ys, galaxy_xs = np.where(image == "#")
+
+expansion_xs = np.where((image == ".").all(axis=0))[0]
+expansion_ys = np.where((image[:, 1:] == image[:, :-1]).all(axis=1))[0]
+
+for x in expansion_xs[::-1]:
+    galaxy_xs[galaxy_xs > x] += 1
+
+for y in expansion_ys[::-1]:
+    galaxy_ys[galaxy_ys > y] += 1
+
+shortest_path_sum = sum(
+    [
+        abs(u[0] - v[0]) + abs(u[1] - v[1])
+        for u, v in combinations(zip(galaxy_ys, galaxy_xs), 2)
+    ]
+)
+
+ic(shortest_path_sum)
+
+exec_time = perf_counter() - timer
+ic(exec_time)
