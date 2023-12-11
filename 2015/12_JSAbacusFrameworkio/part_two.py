0bc0b3b33caaa0960a8dfcf60b146f2a163a9725
Luunynliny <thomasmattone.perso@gmail.com>
2023-12-11 22:13:30 +0100

    day 12 part two

---
+++ 2015/12_JSAbacusFrameworkio/part_two.py
@@ -0,0 +1,28 @@
+import json
+import re
+from time import perf_counter
+
+from icecream import ic
+
+puzzle = "./puzzle.txt"
+
+timer = perf_counter()
+
+with open(puzzle) as f:
+    line = f.read()
+
+
+def hook(obj):
+    return {} if "red" in obj.values() else obj
+
+
+without_red = str(json.loads(line, object_hook=hook))
+
+number_sum_without_red = sum(
+    [int(n) for n in re.findall(r"-?(?:\d+)+", without_red)]
+)
+
+ic(number_sum_without_red)
+
+exec_time = perf_counter() - timer
+ic(exec_time)
