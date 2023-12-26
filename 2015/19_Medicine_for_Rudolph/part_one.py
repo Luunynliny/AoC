3f188c8cafe5df60a7e464c3af6707f7c7bd1b13
Luunynliny <thomasmattone.perso@gmail.com>
2023-12-26 16:51:37 +0100

    day 19 part one

---
+++ 2015/19_Medicine_for_Rudolph/part_one.py
@@ -0,0 +1,45 @@
+import re
+from time import perf_counter
+
+from icecream import ic
+
+puzzle = "./puzzle.txt"
+
+timer = perf_counter()
+
+with open(puzzle) as f:
+    olds = []
+    news = []
+
+    skip = False
+
+    for line in f.read().splitlines():
+        if line == "":
+            skip = True
+
+        if skip:
+            seed = line
+            continue
+
+        o, n = line.split(" => ")
+
+        olds.append(o)
+        news.append(n)
+
+distinct_molecules_cnt = len(
+    set(
+        [
+            r
+            for i in range(len(olds))
+            for r in [
+                seed[: m.start()] + news[i] + seed[m.end() :]
+                for m in re.finditer(olds[i], seed)
+            ]
+        ]
+    )
+)
+
+ic(distinct_molecules_cnt)
+
+exec_time = perf_counter() - timer
+ic(exec_time)
